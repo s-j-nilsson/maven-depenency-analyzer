97be2e8df455d2a48c5f3b78e88ef8fe8a71f10c
Johan Nilsson <s.j.nilsson@gmail.com>
2017-03-11 12:24:44 +0100

    Live filter

--- src/main/java/se/mdh/tools/rest/AnalyzerController.java
+++ src/main/java/se/mdh/tools/rest/AnalyzerController.java
@@ -7,16 +7,17 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import se.mdh.tools.dto.DependencyResponse;
 import se.mdh.tools.service.AnalyzerService;
 
@@ -28,9 +29,6 @@ import se.mdh.tools.service.AnalyzerService;
 public class AnalyzerController {
   private static final Log log = LogFactory.getLog(AnalyzerController.class);
 
-  @Value("${analyzer.jenkins.jobnames.filter:}")
-  String jobnameFilter;
-
   private AnalyzerService analyzerService;
 
   public AnalyzerController(AnalyzerService analyzerService) {
@@ -38,17 +36,21 @@ public class AnalyzerController {
   }
 
   @RequestMapping("")
-  public String printDependenciesTable(Model model) throws IOException, URISyntaxException {
-    populateModel(model);
+  public String printDependenciesTable(Model model, @RequestParam("searchfilter") Optional<String> searchfilter) throws IOException, URISyntaxException {
+    if(searchfilter.isPresent()) {
+      populateModel(model, searchfilter.get());
+    } else {
+      populateModel(model, "");
+    }
     return "table";
   }
 
-  private void populateModel(Model model) throws IOException {
+  private void populateModel(Model model, String searchfilter) throws IOException {
     long start = System.currentTimeMillis();
     List<String> filteredList = new ArrayList<>();
 
-    if(isFiltered()) {
-      filteredList = Arrays.asList(jobnameFilter.split("\\s*,\\s*"));
+    if(isFiltered(searchfilter)) {
+      filteredList = Arrays.asList(searchfilter.split("\\s*,\\s*"));
     }
 
     DependencyResponse dependencyResponse = analyzerService.getDependencies(filteredList);
@@ -57,29 +59,29 @@ public class AnalyzerController {
     log.info("Getting files from Jenkins took " + (stop - start) + " ms");
 
     model.addAttribute("dependencies", dependencyResponse.getDependencyMap());
-    model.addAttribute("isFiltered", isFiltered());
-    model.addAttribute("filter", jobnameFilter);
-    model.addAttribute("missingFilter", getJobNamesWithoutDependenciesExposed(dependencyResponse));
+    model.addAttribute("isFiltered", isFiltered(searchfilter));
+    model.addAttribute("filter", searchfilter);
+    model.addAttribute("missingFilter", getJobNamesWithoutDependenciesExposed(dependencyResponse, searchfilter));
   }
 
   /**
    * Filter on specific Jenkins job names?
    * @return
    */
-  public boolean isFiltered() {
+  public boolean isFiltered(String searchfilter) {
     boolean filtered = false;
 
-    if(!StringUtils.isEmpty(jobnameFilter)) {
+    if(!StringUtils.isEmpty(searchfilter)) {
       filtered = true;
     }
     return filtered;
   }
 
-  private String getJobNamesWithoutDependenciesExposed(DependencyResponse dependencyResponse) {
+  private String getJobNamesWithoutDependenciesExposed(DependencyResponse dependencyResponse, String searchfilter) {
     Map<String, Boolean> matchingMap = new HashMap<>();
 
     List<String> jobsIncluded = dependencyResponse.getJobsIncluded();
-    List<String> jobNameFilterList = Arrays.asList(jobnameFilter.split("\\s*,\\s*"));
+    List<String> jobNameFilterList = Arrays.asList(searchfilter.split("\\s*,\\s*"));
 
     for(String jobNameFilter : jobNameFilterList) {
       matchingMap.put(jobNameFilter, Boolean.FALSE);
